6a3736492561ce1c29923f3cd22b627776153a6e
Jonathan Kho <yja6qa@virginia.edu>
2024-11-13 09:59:33 -0500

    Read in TNG, now working on Illustris comparison

--- M_Sigma.py
+++ M_Sigma.py
@@ -20,24 +20,25 @@ import pickle
 # Path to simulation data
 path_to_output='/standard/torrey-group/BRAHMA/L12p5n512' # this is the folder containing the simulation run
 run='/AREPO/' # name of the simulation runs
-output='output_ratio10_SFMFGM5_seed5.00_bFOF_LW10_spin_rich/'
+output='output_ratio10_SFMFGM5_seed5.00_bFOF/'
 basePath = path_to_output+run+output
 
 file_format='fof_subfind'
 
-desired_redshift=5
+desired_redshift=0
 h = 0.6774 # hubble constant 
 
 M = []
+MStarsBH=[]
 MStars=[]
 ZStars=[]
 SFR=[]
-Sigmas = []
-# SigmaCOM = []
+SigmasBH = [] # Sigma for subhalos with a BH and with > 10 stars
+SigmaStars = [] # Sigma for subhalos with > 10 stars
 VelsMagBHs = []
 VelsMagCOMs = []
 VelBHs = []
-VelCOMs = []
+VelCOMs = [] # Stellar velocities with subhalo COM subtracted
 NStars = []
 BH_Progs=[]
 BH_Mdots=[]
@@ -50,13 +51,15 @@ SubhaloLenType,o = arepo_package.get_subhalo_property(basePath,'SubhaloLenType',
 SubhaloBHLen = SubhaloLenType[:,5]
 SubhaloStarsLen = SubhaloLenType[:,4]
 SubhaloIndices = np.arange(0,len(SubhaloBHLen))
-mask = np.logical_and(SubhaloBHLen>0,SubhaloStarsLen>0)  # Only subhalos with a BH and with stars
-SubhaloIndicesWithStars = SubhaloIndices[mask]
+mask1 = np.logical_and(SubhaloBHLen>0,SubhaloStarsLen>10)  # Only subhalos with a BH and with stars
+mask2 = SubhaloStarsLen>10                # Only subhalos with stars; want another array of values for a Mstar-msigma plot
+
+SubhaloIndicesWithBH = SubhaloIndices[mask1] # Return these so we can cross-reference which subhalos to plot
+SubhaloIndicesWithStars = SubhaloIndices[mask2]
 
 desired_indices = range(len(SubhaloIndicesWithStars))
 
-SubhaloArrayIndex=SubhaloIndicesWithStars
-
+# Get BH IDs so we can store BH array index later, see line 140
 AllBHIDs = arepo_package.get_particle_property(basePath,'ParticleIDs',5,desired_redshift)
 
 
@@ -86,46 +89,57 @@ for index in desired_indices:
     ActualSubhaloIndex = SubhaloIndicesWithStars[index]
     Vel_subhalo,Vel_group,output_redshift=brahma_analysis.get_particle_property_within_postprocessed_groups_adj(basePath,'Velocities',4,output_redshift,ActualSubhaloIndex,requested_property1,store_all_offsets=1,group_type='subhalo')
     MStars_subhalo,Mstars_group,output_redshift=brahma_analysis.get_particle_property_within_postprocessed_groups_adj(basePath,'Masses',4,output_redshift,ActualSubhaloIndex,requested_property2,store_all_offsets=1,group_type='subhalo')
-    BHMasses_subhalo,BHMasses_group,output_redshift=brahma_analysis.get_particle_property_within_postprocessed_groups_adj(basePath,'Masses',5,desired_redshift,ActualSubhaloIndex,requested_property3,store_all_offsets=1,group_type='subhalo')
-    BHProgs_subhalo,BHProgs_group,output_redshift=brahma_analysis.get_particle_property_within_postprocessed_groups_adj(basePath,'BH_Progs',5,desired_redshift,ActualSubhaloIndex,requested_property4,store_all_offsets=1,group_type='subhalo')
-    BHID_subhalo,BHID_group,output_redshift=brahma_analysis.get_particle_property_within_postprocessed_groups_adj(basePath,'ParticleIDs',5,desired_redshift,ActualSubhaloIndex,requested_property5,store_all_offsets=1,group_type='subhalo')
-    BHMdot_subhalo,BHMdot_group,output_redshift=brahma_analysis.get_particle_property_within_postprocessed_groups_adj(basePath,'BH_Mdot',5,desired_redshift,ActualSubhaloIndex,requested_property6,store_all_offsets=1,group_type='subhalo')
-    BH_vel_subhalo,BH_vel_subhalo_group,output_redshift=brahma_analysis.get_particle_property_within_postprocessed_groups_adj(basePath,'Velocities',5,desired_redshift,ActualSubhaloIndex,requested_property7,store_all_offsets=1,group_type='subhalo')
     
-        
-    # Velocity calculations
-    N = len(Vel_subhalo) # number of stars
-    Vel_BH = np.array(Vel_subhalo)/np.sqrt(a) - BH_vel_subhalo[0]/np.sqrt(a) # New units: km/s
-    Vel_CM = np.array(Vel_subhalo)/np.sqrt(a) - SubhaloCMvel[ActualSubhaloIndex]   # New units: km/s
-        
-    VelocityMagBH = np.linalg.norm(Vel_BH, axis=1) # Calculate velocity magnitudes
-    VelocityMagCM = np.linalg.norm(Vel_CM, axis=1)
-    mu_BH = np.mean(Vel_BH,axis=0) # Average 3D stellar velocity for this subhalo
     Mstars_total = np.sum(MStars_subhalo)
     
-    # Here we weight the sigma calculation by stellar mass
-    BHDiffSquared=MStars_subhalo[:, np.newaxis]*np.array((Vel_BH - mu_BH)** 2)
+    # Velocity calculations
+    N = len(Vel_subhalo) # number of stars
 
-    Sigma = np.sqrt(np.sum(BHDiffSquared,axis=0) / Mstars_total)  # Calculate sigma from subhalo velocity
+    Vel_CM = np.array(Vel_subhalo)/np.sqrt(a) - SubhaloCMvel[ActualSubhaloIndex]   # New units: km/s
+    VelocityMagCM = np.linalg.norm(Vel_CM, axis=1)
+    mu_CM = np.mean(Vel_CM,axis=0) # Average 3D stellar velocity for this subhalo
     
-    M.append(np.max(BHMasses_subhalo)*1e10*h)
+    # Here we weight the sigma calculation by stellar mass
+    CMDiffSquared=MStars_subhalo[:, np.newaxis]*np.array((Vel_CM - mu_CM)** 2)
+    
+    Sigma = np.sqrt(np.sum(CMDiffSquared,axis=0) / Mstars_total)    # Calculate sigma from subhalo velocity
+    
+    # Add all of our current subhalo values to our lists
     MStars.append(MStars_subhalo*1e10*h)
-    ZStars.append(SubhaloZStars[index])
-    SFR.append(SubhaloSFR[index])
-    Sigmas.append(Sigma) 
-    VelsMagBHs.append(VelocityMagBH)
+    ZStars.append(SubhaloZStars[ActualSubhaloIndex])
+    SFR.append(SubhaloSFR[ActualSubhaloIndex])
+    SigmaStars.append(Sigma) 
     VelsMagCOMs.append(VelocityMagCM)
-    VelBHs.append(Vel_BH)
     VelCOMs.append(Vel_CM)
-    
-    
     NStars.append(N)
-    BH_Progs.append(BHProgs_subhalo[0])
-    BH_Mdots.append(BHMdot_subhalo[0])
-    BHIDs.append(BHID_subhalo[0])
-    BHArrayIndex.append(np.where(AllBHIDs[0] == BHID_subhalo[0])[0][0])
-    ZGas.append(SubhaloZGas[index])
-    IgnoredBhs += len(BHMasses_subhalo)-1
+    ZGas.append(SubhaloZGas[ActualSubhaloIndex])
     
     
-brahma_analysis.Write2File(M,MStars,ZStars,SFR,Sigmas,VelsMagBHs,VelsMagCOMs,VelBHs,VelCOMs,NStars,BH_Progs,BH_Mdots,BHIDs,BHArrayIndex,ZGas,IgnoredBhs,SubhaloArrayIndex,fname='output_ratio10_SFMFGM5_seed5.00_bFOF_LW10_spin_rich_z5')
+    # If the subhalo also meets the BH criteria
+    if ActualSubhaloIndex in SubhaloIndicesWithBH:
+        BHMasses_subhalo,BHMasses_group,output_redshift=brahma_analysis.get_particle_property_within_postprocessed_groups_adj(basePath,'Masses',5,desired_redshift,ActualSubhaloIndex,requested_property3,store_all_offsets=1,group_type='subhalo')
+        BHProgs_subhalo,BHProgs_group,output_redshift=brahma_analysis.get_particle_property_within_postprocessed_groups_adj(basePath,'BH_Progs',5,desired_redshift,ActualSubhaloIndex,requested_property4,store_all_offsets=1,group_type='subhalo')
+        BHID_subhalo,BHID_group,output_redshift=brahma_analysis.get_particle_property_within_postprocessed_groups_adj(basePath,'ParticleIDs',5,desired_redshift,ActualSubhaloIndex,requested_property5,store_all_offsets=1,group_type='subhalo')
+        BHMdot_subhalo,BHMdot_group,output_redshift=brahma_analysis.get_particle_property_within_postprocessed_groups_adj(basePath,'BH_Mdot',5,desired_redshift,ActualSubhaloIndex,requested_property6,store_all_offsets=1,group_type='subhalo')
+        BH_vel_subhalo,BH_vel_subhalo_group,output_redshift=brahma_analysis.get_particle_property_within_postprocessed_groups_adj(basePath,'Velocities',5,desired_redshift,ActualSubhaloIndex,requested_property7,store_all_offsets=1,group_type='subhalo')
+        
+        Vel_BH = np.array(Vel_subhalo)/np.sqrt(a) - BH_vel_subhalo[0]/np.sqrt(a) # New units: km/s
+        VelocityMagBH = np.linalg.norm(Vel_BH, axis=1) # Calculate velocity magnitudes
+        mu_BH = np.mean(Vel_BH,axis=0) # Average 3D stellar velocity for this subhalo
+        
+        BHDiffSquared=MStars_subhalo[:, np.newaxis]*np.array((Vel_BH - mu_BH)** 2)
+        SigmaBH = np.sqrt(np.sum(BHDiffSquared,axis=0) / Mstars_total)
+        
+        # Add out BH properties to our lists
+        M.append(np.max(BHMasses_subhalo)*1e10*h)
+        SigmasBH.append(SigmaBH)
+        MStarsBH.append(MStars_subhalo*1e10*h)
+        VelsMagBHs.append(VelocityMagBH)
+        VelBHs.append(Vel_BH)
+        BH_Progs.append(BHProgs_subhalo[0])
+        BH_Mdots.append(BHMdot_subhalo[0])
+        BHIDs.append(BHID_subhalo[0])
+        BHArrayIndex.append(np.where(AllBHIDs[0] == BHID_subhalo[0])[0][0]) # Store BH array index
+        IgnoredBhs += len(BHMasses_subhalo)-1
+    
+brahma_analysis.Write2File(M,MStarsBH,MStars,ZStars,SFR,SigmasBH,SigmaStars,VelsMagBHs,VelsMagCOMs,VelBHs,VelCOMs,NStars,BH_Progs,BH_Mdots,BHIDs,BHArrayIndex,ZGas,IgnoredBhs,SubhaloIndicesWithStars,SubhaloIndicesWithBH,fname='Brahma_Data/output_ratio10_SFMFGM5_seed5.00_bFOF_z0')
--- Read_TNG.py
+++ Read_TNG.py
@@ -23,65 +23,86 @@ import pickle
 # offsets: /standard/torrey-group/IllustrisTNG/Runs/L75n1820TNG/postprocessing/offsets/
 
 TNGpath='/standard/torrey-group/IllustrisTNG/Runs/L75n1820TNG'
+Illustrispath='/standard/torrey-group/Illustris/Runs/L75n1820FP'
 
-group=99
+group=135 # z=0 in Illustris
 fields=['SubhaloLenType']
-subhalos = il.groupcat.loadSubhalos(TNGpath,group,fields=fields)
+subhalos = il.groupcat.loadSubhalos(Illustrispath,group,fields=fields)
 
 SubhaloBHLen = subhalos[:,5]
 SubhaloStarsLen = subhalos[:,4]
 SubhaloIndices = np.arange(0,len(SubhaloBHLen))
-mask = np.logical_and(SubhaloBHLen>0,SubhaloStarsLen>0)  # Only subhalos with a BH and with stars
-SubhaloIndicesWithStars = SubhaloIndices[mask]
-desired_indices = range(len(SubhaloIndicesWithStars))
+mask1 = np.logical_and(SubhaloBHLen>0,SubhaloStarsLen>10)  # Only subhalos with a BH and with 10 stars
+mask2 = SubhaloStarsLen>10
+SubhaloIndicesWithStars = SubhaloIndices[mask2]
+SubhaloIndicesWithBH = SubhaloIndices[mask1]
+
+SubhaloCM_vel = il.groupcat.loadSubhalos(Illustrispath,group,fields='SubhaloVel')
 
 h = 0.6774 # hubble constant 
-
-# Load in TNG BH properties
-fields=['Masses','Velocities']
-BHProp = il.snapshot.loadSubset(TNGpath,99,5,fields=fields)
-
-BHMass=BHProp['Masses']
-BHVel=BHProp['Velocities']
-
-# Load in TNG Star velocities
-fields=['Masses','Velocities']
-StarsProp=il.snapshot.loadSubset(TNGpath,99,4,fields=fields)
-
-StarMass=StarsProp['Masses']
-StarVel=StarsProp['Velocities']
-
-Sigmas=[]
-BHMass=[]
-Mstars=[]
 
 output_redshift=0
 a = 1/(1+output_redshift)
 
-for index in desired_indices:
+SigmasCM=[]
+SigmasBH=[]
+BHMass=[]
+MStarsBH=[] # Mass of stars with a BH
+MStars=[]
+NStars=[]
+VelStarsBH=[]
+VelStars=[]
+
+bad_indices=np.arange(16940,16944,1) # For some reason these Illustris subhalos won't load in
+
+mask = np.isin(SubhaloIndicesWithStars, bad_indices, invert=True) # Removing these troublesome subhalos from our list manually
+SubhaloIndicesWithStars=SubhaloIndicesWithStars[mask]
+
+for index in SubhaloIndicesWithStars:
+        
+    # We want to read in stellar and BH masses and velocities
+    fields = ['Masses','Velocities']
+    Star_Props = il.snapshot.loadSubhalo(Illustrispath, snapNum=group, id=index, partType=4, fields=fields)
     
-    ActualSubhaloIndex = SubhaloIndicesWithStars[index]
+    Star_Mass=Star_Props['Masses']
+    Star_Vel=Star_Props['Velocities']
     
-    Vel_Star_subhalo = StarVel[ActualSubhaloIndex]
-    M_Stars_subhalo = StarMass[ActualSubhaloIndex]
+    N = len(Star_Vel) # number of stars
+    Mstars_total = np.sum(Star_Mass) # Total stellar mass
     
-    Vel_BH_subhalo = BHVel[ActualSubhaloIndex]
-    M_BH_subhalo = BHMass[ActualSubhaloIndex] # Got an index out of range error
-    
-    N = len(Vel_Star_subhalo) # number of stars
-    local_vel = np.array(Vel_Star_subhalo)/np.sqrt(a) - Vel_BH_subhalo[0]/np.sqrt(a) # New units: km/s    
-    
-    # Velocity_mag_local = np.linalg.norm(local_vel, axis=1) # Calculate velocity magnitudes
-    mu_BH = np.mean(local_vel,axis=0) # Average 3D stellar velocity for this subhalo
-    Mstars_total = np.sum(M_Stars_subhalo)
+    # Subtract subhalo COM velocity to get local velocities
+    local_vel_CM = np.array(Star_Vel)/np.sqrt(a) - SubhaloCM_vel[index]/np.sqrt(a) # New units: km/s 
     
     # Here we weight the sigma calculation by stellar mass
-    BHDiffSquared=M_Stars_subhalo[:, np.newaxis]*np.array((local_vel - mu_BH)** 2)
+    mu_CM = np.mean(local_vel_CM,axis=0) # Average 3D stellar velocity for this subhalo
+    CMDiffSquared=Star_Mass[:, np.newaxis]*np.array((local_vel_CM - mu_CM)** 2)
     
-    Sigma = np.sqrt(np.sum(BHDiffSquared,axis=0) / Mstars_total)  # Calculate sigma from subhalo velocity
+    SigmaCM = np.sqrt(np.sum(CMDiffSquared,axis=0) / Mstars_total)  # Calculate sigma from subhalo velocity
     
-    Sigmas.append(Sigma)
-    BHMass.append(np.max(M_BH_subhalo)*1e10*h)
-    Mstars.append(M_Stars_subhalo*1e10*h)
+    # If our subhalo has a BH, we store its properties as well
+    if index in SubhaloIndicesWithBH:
+        
+        BH_Props = il.snapshot.loadSubhalo(Illustrispath, snapNum=group, id=index, partType=5, fields=fields)
+
+        BH_Mass=BH_Props['Masses']
+        BH_Vel=BH_Props['Velocities']
+        
+        Massive_BH_vel = BH_Vel[np.argmax(BH_Mass)] # Take the most massive BH's vel to subtract stellar vel from
+        local_vel_BH = np.array(Star_Vel)/np.sqrt(a) - Massive_BH_vel/np.sqrt(a) # New units: km/s   
     
-brahma_analysis.Write2File(BHMass,Mstars,Sigmas,fname='Brahma_Data/TNG_z0')
+        mu_BH = np.mean(local_vel_BH,axis=0) # Average 3D stellar velocity for this subhalo
+        BHDiffSquared=Star_Mass[:, np.newaxis]*np.array((local_vel_BH - mu_BH)** 2)
+    
+        SigmaBH = np.sqrt(np.sum(BHDiffSquared,axis=0) / Mstars_total)  # Calculate sigma from subhalo velocity
+    
+        SigmasBH.append(SigmaBH)
+        BHMass.append(np.max(BH_Mass)*1e10*h) # Add most massive BH mass to our list
+        MStarsBH.append(Star_Mass*1e10*h)
+        VelStarsBH.append(local_vel_BH)
+    
+    SigmasCM.append(SigmaCM)
+    MStars.append(Star_Mass*1e10*h)
+    NStars.append(N)
+    VelStars.append(local_vel_CM)
+    
+brahma_analysis.Write2File(BHMass,MStars,MStarsBH,SigmasCM,SigmasBH,NStars,VelStars,VelStarsBH,fname='Brahma_Data/Illustris_z0')
